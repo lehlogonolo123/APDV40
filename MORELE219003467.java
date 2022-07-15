3d75c9f32f5dcd3e130bac5e2d77e84d61908f1f
lehlogonolo123 <107518750+lehlogonolo123@users.noreply.github.com>
2022-07-15 11:37:15 +0200

    Add files via upload

---
+++ MORELE219003467.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbers{
+    public static void main(String[] args){
+
+      int num1 = 3, num2 =7,sum;
+      sum = num1 + num2;
+    System.out.println ("Sum of these numbers: "+sum);
+  }
+}
